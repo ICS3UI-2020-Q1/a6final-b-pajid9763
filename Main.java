9c70a0c40089e7d62e0ecd2ffe9bdc2558dd510b
pajid9763 <pajid9763@wrdsb.ca>
2020-10-09 13:29:04 +0000

    finished program that check if arrays are the same

--- Main.java
+++ Main.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program that checks if two arrays are the same
+ * @author Darian
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    int array1[] = new int[5];
+    int array2[] = new int[5];
+
+    // ask the user for 5 numbers for the first array
+    System.out.println("Please enter the values for the first array:");
+    for (int i = 0; i < array1.length; i++) {
+      array1[i] = input.nextInt();
+    }
+
+    // ask the user for 5 numbers for the second array
+    System.out.println("Please enter the values for the second array:");
+    for (int i = 0; i < array2.length; i++) {
+      array2[i] = input.nextInt();
+    }
+
+    // declare a count variable to keep count of how many numbers are the same
+    int count = 0;
+
+    // check each number for each array
+    for (int i = 0; i < array1.length; i++) {
+      if (array1[i] == array2[i]) {
+        count++;
+      }
+    }
+    // if every number for both arrays are the same
+    if (count == 5) {
+      System.out.println("These arrays are the same");
+
+      // if 1 or more numbers are not the same
+    } else {
+      System.out.println("These arrays are not the same");
+    }
   }
 }
